7258c9d71da568d9a5b148490bbcb662948ed843
krish <vkmax975@gmail.com>
2024-07-14 01:51:50 +0530

    StreamAPi Examples July14

--- src/main/java/org/java8/utils/EmployeeRepository.java
+++ src/main/java/org/java8/utils/EmployeeRepository.java
@@ -2,17 +2,18 @@ package org.java8.utils;
 
 
 import java.util.List;
+import java.util.Objects;
 
 public class EmployeeRepository {
     private static List<Employee> empList;
 
     public EmployeeRepository(List<Employee> empList) {
-        this.empList = empList;
+        EmployeeRepository.empList = empList;
 
     }
     public static Employee findById(Integer id) {
         for (Employee emp : empList) {
-            if (emp.getId() == id) {
+            if (Objects.equals(emp.getId(), id)) {
                 return emp;
             }
         }
